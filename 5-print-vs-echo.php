39622456a7685649a64bf818ed283e0fdcf7303e
saifur703 <saifur703@gmail.com>
2020-05-14 05:06:46 +0600

    print, concat, difference between print and echo

---
+++ 5-print-vs-echo.php
@@ -0,0 +1,12 @@
+<?php
+
+/** 
+ * concat
+ * different between print and echo 
+ */
+$fName = "Saifur";
+$lName = "Rahman";
+$fullName = $fName . " " . $lName; // concat 
+echo $fullName . "<br/>";
+
+echo "Hello \"BD\"";
