a66b85cbbd92141010914d118d0f392dfbcfd614
saifur703 <saifur703@gmail.com>
2020-05-15 00:50:51 +0600

    Login System by _SESSON

---
+++ login/dashboard.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    header("location: index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <h1>Welcome to Dashboard</h1>
+
+                <p>Your Email is: <?php echo $_SESSION['email']; ?></p>
+
+
+                <a href="logout.php">Logout</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ login/index.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+if (isset($_SESSION['email'])) {
+    header("location: dashboard.php");
+}
+
+if (isset($_POST['login'])) {
+    define("EMAIL", "saifur703@gmail.com");
+    define("PASSWORD", "1234");
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if ($email == EMAIL) {
+        if ($password == PASSWORD) {
+            $_SESSION['email'] = $email;
+
+            header("location: dashboard.php");
+        } else {
+            $pass = "<p style='color: red;'>Password is not matching. Please Try again</p>";
+        }
+    } else {
+        $em = "<p style='color: red;'>Email is not matching. Please Try again</p>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
+
+    <link rel="stylesheet" href="login/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <div class="login-box">
+                    <h2>Login </h2>
+
+                    <form action="" method="POST">
+
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" placeholder="Your Email">
+                        <?php if ($email != EMAIL) {
+                            echo $em;
+                        } ?>
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Your Password">
+                        <?php if ($password != PASSWORD) {
+                            echo $pass;
+                        } ?>
+                        <button class="button" name="login">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
