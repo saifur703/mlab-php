5d005a4879ded1e375e7582c4492a294ebd51c99
saifur703 <saifur703@gmail.com>
2020-05-14 06:13:31 +0600

    Condition - if, else if, else

---
+++ 7-condition.php
@@ -0,0 +1,18 @@
+<?php
+
+$x = 5;
+if ($x == 5) {
+    echo "Yes";
+} else {
+    echo "No";
+}
+
+echo "<br/>";
+
+if ($x % 2 == 0) {
+    echo "Even Number";
+} else if ($x % 2 == 1) {
+    echo "Odd Number";
+} else {
+    echo "Invalid";
+}
---
+++ 8-condition.php
@@ -0,0 +1,10 @@
+<?php
+
+$num = 56;
+if ($num >= 33 && $num <= 100) {
+    echo "Pass";
+} else if ($num >= 0 && $num <= 32) {
+    echo "Fail";
+} else {
+    echo "Invalid Result";
+}
