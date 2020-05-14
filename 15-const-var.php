9b4ee13d8ab4477f7b358b508f9b14031c737559
saifur703 <saifur703@gmail.com>
2020-05-14 22:55:03 +0600

    variable vs constant

---
+++ 15-const-var.php
@@ -0,0 +1,21 @@
+<?php
+
+/** 
+ * constant 
+ * variable
+ */
+
+// variable
+$x = 10;
+$x = 24;
+echo $x;
+
+echo PHP_EOL;
+
+// constant 
+define("NUM", 12);
+echo NUM;
+
+echo PHP_EOL;
+
+echo __FILE__;
