bbc035801e056b790137830f8f4ddc30505b643b
saifur703 <saifur703@gmail.com>
2020-05-15 20:31:33 +0600

    Switch Case

---
+++ 28-switch-case.php
@@ -0,0 +1,48 @@
+<?php
+$x = 2;
+switch ($x) {
+    case 0:
+        echo "Zero";
+        break;
+    case 1:
+        echo "One";
+        break;
+
+    case 2:
+        echo "Two";
+        break;
+
+    case 3:
+        echo "Three";
+        break;
+
+    case 4:
+        echo "Four";
+        break;
+
+    case 5:
+        echo "Five";
+        break;
+
+    case 6:
+        echo "Six";
+        break;
+
+    case 7:
+        echo "Seven";
+        break;
+
+    case 8:
+        echo "Eight";
+        break;
+
+    case 9:
+        echo "Nine";
+        break;
+
+    case 10:
+        echo "Ten";
+        break;
+    default:
+        echo "Anything else!";
+}
